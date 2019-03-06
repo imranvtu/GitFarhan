569b91e19da50a8f06f74f649700a98f19651757
Imran <imran.vtu@gmail.com>
2019-03-06 10:10:35 +0530

    First Git Commit

---
+++ src/test/java/qaclickacademy/RESRAPITest.java
@@ -0,0 +1,24 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RESRAPITest {
+	
+	
+	@Test
+	public void PostJIRA()
+	{
+		System.out.println("PostJIRA");
+	}
+	
+	
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+	
+	
+	
+
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,22 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+	}
+	
+	
+	@Test
+	public void ElelmentsUI()
+	{
+		System.out.println("ElelmentsUI");
+	}
+	
+	
+}
